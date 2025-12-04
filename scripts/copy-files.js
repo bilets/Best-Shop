1ca56b788fa474e7e757c3f28ec5b1e14838d023
Volodymyr <biletskiy1780@gmail.com>
2025-12-04 18:18:49 +0200

    Update favicon to use .ico format in assets folder

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -37,7 +37,6 @@ async function build() {
 
     const filesToCopy = [
       { src: path.join(srcDir, 'index.html'), dest: path.join(distDir, 'index.html') },
-      { src: path.join(srcDir, 'favicon.svg'), dest: path.join(distDir, 'favicon.svg') },
     ];
 
     for (const { src, dest } of filesToCopy) {
